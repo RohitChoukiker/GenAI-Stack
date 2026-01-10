4038887d4a59202377513895dc3f500ac905eec4
RohitChoukiker <rohitchoukiker2803@gmail.com>
2026-01-10 08:58:57 +0530

    add tailwindcss

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
 
   return (
-   <h1>Hello World</h1>
+    <div className="min-h-screen bg-green-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">Hello World</h1>
+    </div>
   )
 }
 
